edde1d14daa3a060835d80edee0f602e64490e5c
laurentiuNedelcu <31955816+laurentiuNedelcu@users.noreply.github.com>
2021-05-21 20:14:31 +0200

    sad

--- insert_default_jsons.py
+++ insert_default_jsons.py
@@ -5,6 +5,7 @@ from influxdb import InfluxDBClient
 from os import listdir
 import os
 import json
+from resources import insert_default_json
 import pandas as pd
 
 
@@ -16,19 +17,21 @@ print(dir_path)
 json_files = [dir_path + f'/{f}' for f in listdir(dir_path)]
 print(json_files)
 
-with open(json_files[-1]) as json_f:
-    # insert_default_json(client, json.load(json_f))
-    print("done")
-    print(json.load(json_f))
 
 # Create new client to connect with InfluxDB
-client = InfluxDBClient(host="localhost", port=8086)
+try:
+    client = InfluxDBClient(host="localhost", port=8086)
+except Exception as e:
+    print(e)
 
-"""
-# Creating a new database
-client.create_database("weather_db")
-print("Database created!")
 
+try:
+    # Creating a new database
+    client.create_database("weather_db")
+    print("Database created!")
+except Exception as e:
+    print(str(e))
+    pass
 # Show databases
 print(client.get_list_database())
 
@@ -38,7 +41,6 @@ client.switch_database("weather_db")
 
 for f in json_files:
     with open(f) as json_f:
-        # insert_default_json(client, json.load(json_f))
+        insert_default_json(client, json.load(json_f))
         print("done")
-        print(pd.DataFrame(json.load(json_f)).head(1))
-"""
+        #print(pd.DataFrame(json.load(json_f)).head(1))
